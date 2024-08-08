cedaa29aac9e2ee3758d9d293b5ecad8b6ef62aa
Julian Chatterton <julian.james.chatterton@gmail.com>
2024-08-08 16:00:58 -0500

    created .env and session secret. updated register  post route to include all fields needed. registration and login working in PostMan

--- database.sql
+++ database.sql
@@ -5,5 +5,8 @@
 CREATE TABLE "user" (
     "id" SERIAL PRIMARY KEY,
     "username" VARCHAR (80) UNIQUE NOT NULL,
-    "password" VARCHAR (1000) NOT NULL
+    "password" VARCHAR (1000) NOT NULL,
+    "email" VARCHAR (100) NOT NULL,
+    "first_name" VARCHAR (100) NOT NULL,
+    "last_name" VARCHAR (100) NOT NULL
 );
