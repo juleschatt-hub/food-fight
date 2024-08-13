9644500a25da3daa0d90bea5d3706b5062550438
Julian Chatterton <julian.james.chatterton@gmail.com>
2024-08-12 21:19:31 -0500

    added some logic to places router get request to randomize the results returned.

--- server/routes/places.router.js
+++ server/routes/places.router.js
@@ -22,14 +22,22 @@ router.get('/', rejectUnauthenticated, async (req, res) => {
       const response = await axios.get('https://maps.googleapis.com/maps/api/place/nearbysearch/json', {
         params: {
             location: '44.9778,-93.2650',
-            radius: 16093.4,
+            radius: 50000,
             type: 'restaurant',
             key: PLACES_API_KEY
         },
       });
-  
+
+    //sorts api call results in a random order
+    const restaurants = response.data.results.sort(() => .5 - Math.random());
+    
+    //after results have been radomized this takes 
+    //the first ten results from the response
+    const randomRestaurants = restaurants.slice(0, 10);
+    console.log('array size', randomRestaurants.length);
+
       // Return the API response to the client
-      res.send(response.data);
+      res.send(randomRestaurants);
     } catch (error) {
       // Handle any errors
       console.error('Error fetching data from Google Places API:', error);
