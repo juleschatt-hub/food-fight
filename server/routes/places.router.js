cdf170ed6dd200bf5e14ed64230c20ee25e97cd3
Julian Chatterton <julian.james.chatterton@gmail.com>
2024-08-13 13:50:47 -0500

    /restaurtans  post route is posting 10 random restaurants to the restaurants table

--- server/routes/places.router.js
+++ server/routes/places.router.js
@@ -35,11 +35,12 @@ router.get('/', rejectUnauthenticated, async (req, res) => {
     //the first ten results from the response
     const randomRestaurants = restaurants.slice(0, 10);
     console.log('array size', randomRestaurants.length);
+    console.log(randomRestaurants);
 
     //isolating photo reference
-    const photos = randomRestaurants.map(i => {
-        let photoReference = i.photos.map(i => i.photo_reference);
-        console.log('photo reference', photoReference)
+    randomRestaurants.map(i => {
+        let photoReference = i.photos[0].photo_reference;
+        console.log('photo reference', photoReference);
     })
     
 
@@ -50,6 +51,56 @@ router.get('/', rejectUnauthenticated, async (req, res) => {
       res.status(500).json({ error: 'Error fetching data from Google Places API' });
     }
   });
+
+  //POST 10 random restaurants to DB
+  router.post('/restaurants', async (req, res) => {
+    try {
+        // Make a request to the Google Places API
+        const response = await axios.get('https://maps.googleapis.com/maps/api/place/nearbysearch/json', {
+          params: {
+              location: '44.9778,-93.2650',
+              radius: 50000,
+              type: 'restaurant',
+              key: PLACES_API_KEY
+          },
+        });
+
+      //sorts api call results in a random order
+      const restaurants = response.data.results.sort(() => .5 - Math.random());
+      
+      //after results have been radomized this takes 
+      //the first ten results from the response
+      const randomRestaurants = restaurants.slice(0, 10);
+      console.log('array size', randomRestaurants.length);
+
+
+       randomRestaurants.map(restaurant => {
+        const photoReference = restaurant.photos[0].photo_reference;
+        const restaurantName = restaurant.name;
+        const placeId = restaurant.place_id;
+        const rating = restaurant.rating;
+        const priceLevel = restaurant.price_level;
+        const dinerLike = false;
+        const guestLike = false;
+        const fightId = 0;
+
+        const queryText = `INSERT INTO "restaurants" (fight_id, restaurant_name, place_id, photo_reference, rating, price_level, diner_like, guest_like)
+        VALUES ($1, $2, $3, $4, $5, $6, $7, $8);`;
+        pool
+        .query(queryText, [fightId, restaurantName, placeId, photoReference, rating, priceLevel, dinerLike, guestLike]) 
+       
+        
+    });
+
+res.sendStatus(201);
+
+}
+    catch(error) {
+        console.error('error in restaurants post router', error);
+        res.sendStatus(500);
+    }
+
+  })
 
 
 
