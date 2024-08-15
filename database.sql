458f062aab1fe1c9e3967da218a2d6cc4e476dab
Julian Chatterton <julian.james.chatterton@gmail.com>
2024-08-15 14:22:53 -0500

    added create tables to db.sql

--- database.sql
+++ database.sql
@@ -10,3 +10,24 @@ CREATE TABLE "user" (
     "first_name" VARCHAR (100) NOT NULL,
     "last_name" VARCHAR (100) NOT NULL
 );
+
+CREATE TABLE "restaurants" (
+	"id" SERIAL PRIMARY KEY,
+	"fight_id" INT REFERENCES "fights",
+	"restaurant_name" VARCHAR(500) NOT NULL,
+	"place_id" VARCHAR(500),
+	"photo_reference" VARCHAR(500),
+	"rating" FLOAT,
+	"price_level" INT,
+	"diner_like" BOOLEAN,
+	"guest_like" BOOLEAN
+	
+);
+
+CREATE TABLE "fights"(
+	"id" SERIAL PRIMARY KEY,
+	"diner_id" INT NOT NULL REFERENCES "user",
+	"guest_id" INT NOT NULL REFERENCES "user",
+	"dinner_date" DATE NOT NULL,
+	"restaurant_match_id" INT
+);
