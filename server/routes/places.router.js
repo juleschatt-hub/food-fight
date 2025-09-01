1b7c79b4b8471a330aa17c85ec76e49c4cd1a6b5
Julian Chatterton <julian.james.chatterton@gmail.com>
2025-09-01 16:24:36 -0500

    cleaned up places router

--- server/routes/places.router.js
+++ server/routes/places.router.js
@@ -15,43 +15,7 @@ const router = express.Router();
 //Places API
 const PLACES_API_KEY = process.env.PLACES_API_KEY;
 
-// router.get('/placesget', rejectUnauthenticated, async (req, res) => {
-//     try {
-//       // Make a request to the Google Places API
-//       const response = await axios.get('https://maps.googleapis.com/maps/api/place/nearbysearch/json', {
-//         params: {
-//             location: '44.9778,-93.2650',
-//             radius: 50000,
-//             type: 'restaurant',
-//             key: PLACES_API_KEY
-//         },
-//       });
 
-//     //sorts api call results in a random order
-//     const restaurants = response.data.results.sort(() => .5 - Math.random());
-    
-//     //after results have been radomized this takes 
-//     //the first ten results from the response
-//     const randomRestaurants = restaurants.slice(0, 10);
-//     console.log('array size', randomRestaurants.length);
-//     console.log(randomRestaurants);
-
-//     //isolating photo reference
-//     randomRestaurants.map(i => {
-//         let photoReference = i.photos[0].photo_reference;
-//         let maxHeight = i.photos[0].height;
-//         console.log('photo reference', photoReference);
-//         console.log('height', maxHeight);
-//     })
-    
-
-//       // Return the API response to the client
-//       res.send(randomRestaurants);
-//     } catch (error) {
-//       console.error('Error fetching data from Google Places API:', error);
-//       res.status(500).json({ error: 'Error fetching data from Google Places API' });
-//     }
-//   });
 
   // GET a list of food fights for the logged in user
   router.get('/userfights', (req, res) => {
